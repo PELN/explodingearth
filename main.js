c9a69ad79c44bb9d4814247033e4f3a1b9f9fc7d
peln <pern9378@stud.kea.dk>
2018-05-24 18:09:05 +0200

    finished

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 
  function loadSVG(path, element, func){
-     fetch(path)
-        .then(function(response){
-            return response.text();
-        })
-        .then(function(data){
-            console.log(document.querySelector(element))
-            document.querySelector(element).innerHTML = data;
-            //
-            func();
-        })
- }
+    fetch(path)
+       .then(function(response){
+           return response.text();
+       })
+       .then(function(data){
+           console.log(document.querySelector(element))
+           document.querySelector(element).innerHTML = data;
+           //
+           func();
+       })
+}
 
- loadSVG("assets/sarah.svg", "div", function(){
-     console.log("sarah loaded");
-     document.querySelector("#booms").style.display="none";
-     
- });
+loadSVG("assets/sarah.svg", "div", function(){
+    console.log("sarah loaded");
+    document.querySelector("#booms").style.display="none";
+
+    let stuff = document.querySelectorAll(".land path, .water path");
+   // console.log(stuff);
+
+   document.querySelector("svg").addEventListener("click", ()=>{
+
+       stuff.forEach((el, i)=>{
+           setTimeout(()=>{
+               el.style.transform = `translate(${Math.random()*100-50}px, ${Math.random()*100-50}px)`;
+           }, i*10)
+       });
+
+   });
+
+});
+
+
+
+
+
+
+
